f2e8bbc4c5923322738c17d2e2bc429f722751ad
Jurgen Van de Moere <jurgen@webberid.com>
2014-03-08 15:39:16 +0100

    Add additional unit tests

--- test/deqoratorSpec.js
+++ test/deqoratorSpec.js
@@ -2,7 +2,6 @@ var chai = require('chai'),
     expect = chai.expect,
     sinonChai = require('sinon-chai'),
     sinon = require('sinon'),
-    events = require('events'),
     Deqorator = require('../lib/deqorator');
 
 chai.use(sinonChai);
@@ -22,10 +21,6 @@ describe('Deqorator', function(){
 
         });
 
-        it('should inherit from EventEmitter', function () {
-            expect(deqorator).to.be.instanceof(events.EventEmitter);
-        });
-
         it('should store middleware correctly', function () {
             var middleware1 = function(){};
             var middleware2 = function(){};
@@ -37,38 +32,78 @@ describe('Deqorator', function(){
 
         it('should execute middleware in the correct order', function (done) {
             var item = {};
-            var middleware1 = function(item, next){
+            var middleware1 = function(err, item, next){
                 item.hello = 'world';
                 next();
             };
-            var middleware2 = function(item, next){
+            var middleware2 = function(err, item, next){
                 expect(item.hello).to.equal('world');
                 next();
             };
             deqorator.use(middleware1);
             deqorator.use(middleware2);
-            deqorator.on('complete', function(item){
+            deqorator.decorate(item, function(err, item){
                 console.log('Complete: ' + item);
                 done();
             });
-            deqorator.decorate(item);
         });
 
-        it('should emit a complete event when finished', function (done) {
-            this.timeout(500);
-            var item = {};
-            var middleware1 = function(item, next){
+
+        it('pass the item correctly in the chain of middleware handlers', function (done) {
+            var original = {};
+            var middleware1 = function(err, item, next){
+                expect(item).to.equal(original);
                 next();
             };
-            var middleware2 = function(item, next){
+            var middleware2 = function(err, item, next){
+                expect(item).to.equal(original);
                 next();
             };
             deqorator.use(middleware1);
             deqorator.use(middleware2);
-            deqorator.on('complete', function(item){
+            deqorator.decorate(original, function(err, item){
                 done();
             });
-            deqorator.decorate(item);
+        });
+
+        it('provide a partially decorated item if an error is raised', function (done) {
+            this.timeout(500);
+            var item = {};
+            var middleware1 = function(err, item, next){
+                item.one = 'done';
+                next();
+            };
+            var middleware2 = function(err, item, next){
+                next('some error');
+            };
+            deqorator.use(middleware1);
+            deqorator.use(middleware2);
+            deqorator.decorate(item, function(err, item){
+                expect(err).to.equal('some error');
+                expect(item.one).to.equal('done');
+                done();
+            });
+        });
+
+        it('skip remaining middleware layers if an error is raised', function (done) {
+            this.timeout(500);
+            var item = {};
+            var middleware1 = function(err, item, next){
+                item.one = 'done';
+                next('some error');
+            };
+            var middleware2 = function(err, item, next){
+                item.two = 'done';
+                next();
+            };
+            deqorator.use(middleware1);
+            deqorator.use(middleware2);
+            deqorator.decorate(item, function(err, item){
+                expect(err).to.equal('some error');
+                expect(item.one).to.equal('done');
+                expect(item.two).to.not.equal('done');
+                done();
+            });
         });
 
     });
